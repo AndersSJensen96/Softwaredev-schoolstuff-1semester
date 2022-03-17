0e55b78d294be2bfc8ce2c60b2c375740ff086f7
Nicolai Renbo Svane Sparvath <nico590j@edu.ucl.dk>
2022-03-17 13:25:02 +0100

    Added exsamples of ClassData and MemberData

---
+++ Test/BlazorCalculator/TestCalculator/PowerOfData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCalculator
+{
+	public class PowerOfData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1, 2 };
+            yield return new object[] { -4, -6 };
+            yield return new object[] { 5, 2 };
+            yield return new object[] { 10, 2 };
+            yield return new object[] { 23425632, 34 };
+            yield return new object[] { int.MinValue, int.MaxValue };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	}
+}
--- Test/BlazorCalculator/TestCalculator/UnitTest1.cs
+++ Test/BlazorCalculator/TestCalculator/UnitTest1.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using Bunit;
 using BlazorDemo.Pages;
+using System.Collections.Generic;
 
 namespace TestCalculator
 {
@@ -18,7 +19,7 @@ namespace TestCalculator
             var calculatorComponent = RenderComponent<Calculator>();
             var input1 = calculatorComponent.Find("#FirstColumn");
             input1.MarkupMatches("<input id=\"FirstColumn\" placeholder=\"Enter First Number\"/>");
-            
+
 
             var Sqr = calculatorComponent.Find("#SquareRoot");
             //Sqr.MarkupMatches("<input id=\"FinalResult\" readonly />");
@@ -71,6 +72,76 @@ namespace TestCalculator
         [InlineData(10, 2)]
         [InlineData(23425632, 34)]
         public void PowerOf(int number, int number2)
+        {
+            //Arrange
+            double ExpectedResult = Math.Pow(number, number2);
+
+
+            var calculatorComponent = RenderComponent<Calculator>();
+            var input1 = calculatorComponent.Find("#FirstColumn");
+            input1.MarkupMatches("<input id=\"FirstColumn\" placeholder=\"Enter First Number\"/>");
+
+            var input2 = calculatorComponent.Find("#SecondColumn");
+            input2.MarkupMatches("<input id=\"SecondColumn\" placeholder=\"Enter Second Number\"/>");
+
+
+            var pwr = calculatorComponent.Find("#PowerOf");
+
+            //Act
+            input1.Change(number);
+            input2.Change(number2);
+            pwr.Click();
+
+
+            //Assert
+            var Final = calculatorComponent.Find("#FinalResult");
+            //Final.MarkupMatches($"<input id=\"FinalResult\" readonly />{ExpectedResult}");
+            Assert.Equal(ExpectedResult, double.Parse(Final.GetAttribute("value")));
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void PowerOfMemberData(int number, int number2)
+        {
+            //Arrange
+            double ExpectedResult = Math.Pow(number, number2);
+
+
+            var calculatorComponent = RenderComponent<Calculator>();
+            var input1 = calculatorComponent.Find("#FirstColumn");
+            input1.MarkupMatches("<input id=\"FirstColumn\" placeholder=\"Enter First Number\"/>");
+
+            var input2 = calculatorComponent.Find("#SecondColumn");
+            input2.MarkupMatches("<input id=\"SecondColumn\" placeholder=\"Enter Second Number\"/>");
+
+
+            var pwr = calculatorComponent.Find("#PowerOf");
+
+            //Act
+            input1.Change(number);
+            input2.Change(number2);
+            pwr.Click();
+
+
+            //Assert
+            var Final = calculatorComponent.Find("#FinalResult");
+            //Final.MarkupMatches($"<input id=\"FinalResult\" readonly />{ExpectedResult}");
+            Assert.Equal(ExpectedResult, double.Parse(Final.GetAttribute("value")));
+        }
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { 1, 2 },
+                new object[] { -4, -6},
+                new object[] { 5, 2},
+                new object[] { 10, 2},
+                new object[] { 23425632, 34},
+                new object[] { int.MinValue, int.MaxValue }
+            };
+
+        [Theory]
+        [ClassData(typeof(PowerOfData))]
+        public void PowerOfClassData(int number, int number2)
         {
             //Arrange
             double ExpectedResult = Math.Pow(number, number2);
